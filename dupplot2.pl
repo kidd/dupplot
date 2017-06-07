0b9f266da5697c8c2e850f540371963fe441cc10
Raimon Grau <raimonster@gmail.com>
2017-06-08 00:52:29 +0200

    Integrate with gnuplot.

--- dupplot2.pl
+++ dupplot2.pl
@@ -1,5 +1,5 @@
 #!/usr/bin/perl
-# Usage ./dupplot.pl file1 file2 >/tmp/data ;
+# Usage ./dupplot.pl file1 file2 ;
 # gnuplot, and type: set title "My Plot";  plot '/tmp/data'
 # Author Raimon Grau <raimonster@gmail.com>. Artistic License v2.0
 
@@ -8,6 +8,7 @@ use warnings;
 use Data::Dumper;
 use Digest::MD5;
 use File::Basename;
+use File::Temp;
 
 sub say {print @_,"\n";}
 
@@ -55,14 +56,23 @@ sub process_file {
   return;
 }
 
-process_file(shift, sub { push @{$h{$_}}, $.;});
-process_file(shift,
-             sub {
-               for (@{$h{$_}}) {
-                 push @tuples, [$. , $_];
-               }
-             });
+sub main {
+  process_file(shift, sub { push @{$h{$_}}, $.;});
+  process_file(shift,
+               sub {
+                 for (@{$h{$_}}) {
+                   push @tuples, [$. , $_];
+                 }
+               });
 
-for my $tuple (@tuples) {
-  say $tuple->[0], " " , $tuple->[1] ;
+
+  my ($ffo, $name) = mkstemp("/tmp/tempXXXXXXXX");
+
+  for my $tuple (@tuples) {
+    #say $tuple->[0], " " , $tuple->[1] ;
+    print $ffo $tuple->[0], " " , $tuple->[1] , "\n";
+  }
+  system(qq|gnuplot -p -e "plot '$name'"|);
 }
+
+main(@ARGV);
