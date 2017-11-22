60f3ec5536bc45beae0aa182ab415177bada91a0
Raimon Grau <raimonster@gmail.com>
2017-11-22 17:31:06 +0000

    Adjusted Code to make it a bit more explicit

--- dupplot.pl
+++ dupplot.pl
@@ -37,7 +37,11 @@ sub extension_for {
   return $ext;
 }
 
+# %h is the hash that accumulates the lines of the first file its keys
+# are an md5 of the line, and the values are lists of line numbers
+# where that line appears.
 my %h = ();
+
 my @tuples = ();
 
 sub process_file {
@@ -55,18 +59,28 @@ sub process_file {
 
     next if /^\s*$/;
     $md5 = Digest::MD5::md5_hex($_);
-    $sub->($md5);
+    $sub->($md5, $.); # pass the md5 of the current line and the line
+                      # number ($.)
   }
   close $fn;
   return;
 }
 
 sub main {
-  process_file(shift, sub { push @{$h{$_}}, $.;});
+  process_file(shift, sub {
+                 # Adds the line number to the value of %h indexed by
+                 # the md5 of the line itself
+                 my ($line, $lnum) =  @_;
+                 push @{$h{$line}}, $lnum;
+               });
   process_file(shift,
                sub {
-                 for (@{$h{$_}}) {
-                   push @tuples, [$. , $_];
+                 # For every line, if it exists in the previous file,
+                 # make a correspondence with each appearance
+                 # (cartesian product)
+                 my ($line, $lnum) =  @_;
+                 for my $other_file_line (@{$h{$line}}) {
+                   push @tuples, [$lnum , $other_file_line];
                  }
                });
 
